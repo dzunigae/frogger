ea3503c3704a5529aab747277921ab2352e17ff5
Cristian <Cristian@Camilo>
2017-10-31 19:51:17 -0500

    Renderable: cargarImagen() y ahora solo una imagen por objeto

--- src/frogger/Renderable.java
+++ src/frogger/Renderable.java
@@ -5,8 +5,12 @@
  */
 package frogger;
 
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
 import java.util.ArrayList;
+import javax.swing.ImageIcon;
 
 /**
  *
@@ -16,12 +20,16 @@ public abstract class Renderable {
     public Renderable(int coordenadaY, String nombreArchivoImagen) {
         this.coordenadaY = coordenadaY;
         this.nombreArchivoImagen = nombreArchivoImagen;
-        this.imagenes = new ArrayList<>(0);
     }
     
     public abstract void moverY();
     
+    public void cargarImagen(){
+        ImageIcon iconoImagen = new ImageIcon(this.nombreArchivoImagen);
+        this.imagen = iconoImagen.getImage();
+    }
+    
     protected int coordenadaY;
     protected String nombreArchivoImagen;
-    protected ArrayList<Image> imagenes;
+    protected Image imagen;
 }
